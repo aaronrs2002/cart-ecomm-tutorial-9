cb8b85825f65794eda2e8906bbe1f138b25973ed
aaronrs2002 <aaronrs2002@yahoo.com>
2023-10-17 21:24:04 -0700

    Update PurchaseLog.js
    adding sum total based on which items are being shown via the filter

--- client/src/components/PurchaseLog.js
+++ client/src/components/PurchaseLog.js
@@ -3,10 +3,8 @@ import react, { useEffect, useState } from "react";
 const PurchaseLog = (props) => {
     let [loaded, setLoaded] = useState(false);
     let [userList, setUserList] = useState([]);
-
     let [search, setSearch] = useState("default");
-
-
+    let sumTotal = 0;
 
     const filter = (whichFilter) => {
         let tempUserSearch = document.querySelector("select[name='selectUser']").value;
@@ -25,6 +23,7 @@ const PurchaseLog = (props) => {
         }
 
         setSearch((search) => tempUserSearch);
+        sumTotal = 0;
 
     }
 
@@ -32,22 +31,27 @@ const PurchaseLog = (props) => {
 
 
     useEffect(() => {
+        console.log("JSON.stringify(props.timeSelected): " + JSON.stringify(props.timeSelected));
         if (props.timeSelected.length > 0 && loaded === false) {
             let tempUsers = [];
+            let tempPrice = 0;
             for (let i = 0; i < props.timeSelected.length; i++) {
                 let userEmail = props.timeSelected[i].saleId.substring(0, props.timeSelected[i].saleId.indexOf(":"));
+                tempPrice = parseInt(tempPrice) + parseInt(props.timeSelected[i].price);
+                console.log("tempPrice: " + tempPrice);
                 if (tempUsers.indexOf(userEmail) === -1) {
                     tempUsers.push(userEmail);
                 }
             }
             setUserList((userList) => tempUsers);
+            sumTotal = tempPrice;
             setLoaded((loaded) => true);
         }
     });
 
     return (
 
-        <div className="row">
+        <div className="row pb-5">
             {userList.length > 0 ?
                 <div className="col-md-12">
                     <h2 className="mt-3">Search Items</h2>
@@ -61,15 +65,28 @@ const PurchaseLog = (props) => {
                     {userList.length > 0 ? userList.map((email, i) => { return <option key={i} value={email}>{email}</option> }) : null}
                 </select>
             </div>
-            <div className="col-md-12 pb-5">
-                <ul className="list-group mb-5">
+            <div className="col-md-12 py-1">
+                <ul className="list-group ">
                     {props.timeSelected ?
                         props.timeSelected.map((sale, i) => {
+
+
+                            if ((sale.itemName + sale.saleId).indexOf(search) !== -1 || search === "default") {
+                                sumTotal = Number(sumTotal) + Number(sale.price);
+                                console.log("sumTotal: " + sumTotal)
+                            }
+
+
                             return <li key={i} className={(sale.itemName + sale.saleId).indexOf(search) !== -1 || search === "default" ? "list-group-item" : "hide"} ><span className="capitalize">{sale.itemName}</span>{" - $" + sale.price} <span className="badge badge-secondary">{sale.saleId}</span></li>
                         })
                         : null}
                 </ul>
-            </div>
+            </div>{sumTotal > 0 ?
+                <div className="col-md-12  mb-5 pb-5 " >
+                    <div className="alert alert-info w-100">
+                        <h3>Total: ${sumTotal.toFixed(2)}</h3>
+                    </div>
+                </div> : null}
         </div>)
 }
 
