79a33007c2ce8710a076256c3a90c1ed128156f7
aaronrs2002 <aaronrs2002@yahoo.com>
2024-05-09 06:34:27 -0700

    Update inventory.js
    added placeholders for sql statement

--- routes/api/inventory.js
+++ routes/api/inventory.js
@@ -26,28 +26,34 @@ router.post("/order-product", checkToken, (req, res) => {
 
 //SERVER SIDE GET INVENTORY LOG FOR SPECIFIC ITEM
 router.get("/:itemName", checkToken, (req, res) => {
-    let sql = `SELECT * FROM inventory WHERE itemName = '${req.params.itemName.replace(/[&\/\\#,+()$~%'"*?|<>{}“]/g, '')}' ORDER BY userTimestamp DESC`;
-    let query = db.query(sql, (err, results) => {
-        if (err) {
-            console.log("Error: " + err);
-        } else {
-            res.send(results);
-        }
-    })
+    //let sql = `SELECT * FROM inventory WHERE itemName = '${req.params.itemName.replace(/[&\/\\#,+()$~%'"*?|<>{}“]/g, '')}' ORDER BY userTimestamp DESC`;
+    let query = db.query(
+        `SELECT * FROM inventory WHERE itemName = ? ORDER BY userTimestamp DESC`,
+        [req.params.itemName],
+        (err, results) => {
+            if (err) {
+                console.log("Error: " + err);
+            } else {
+                res.send(results);
+            }
+        })
 });
 
 
 //SERVER SIDE UPDATE ORDER STATUS
 
 router.put("/update-order-status", checkToken, (req, res) => {
-    let sql = `UPDATE inventory SET status = '${req.body.status.replace(/[&\/\\#,+()$~%'"*?|<>{}“]/g, '')}' WHERE userTimestamp = '${req.body.userTimestamp.replace(/[&\/\\#,+()$~%'"*?|<>{}“]/g, '')}' ORDER BY userTimestamp DESC`;
-    let query = db.query(sql, (err, result) => {
-        if (err) {
-            console.log("Error: " + err);
-        } else {
-            res.send(result);
-        }
-    });
+    // let sql = `UPDATE inventory SET status = '${req.body.status.replace(/[&\/\\#,+()$~%'"*?|<>{}“]/g, '')}' WHERE userTimestamp = '${req.body.userTimestamp.replace(/[&\/\\#,+()$~%'"*?|<>{}“]/g, '')}' ORDER BY userTimestamp DESC`;
+    let query = db.query(
+        `UPDATE inventory SET status = ? WHERE userTimestamp = ? ORDER BY userTimestamp DESC`,
+        [req.body.status, req.body.userTimestamp],
+        (err, result) => {
+            if (err) {
+                console.log("Error: " + err);
+            } else {
+                res.send(result);
+            }
+        });
 })
 
 
